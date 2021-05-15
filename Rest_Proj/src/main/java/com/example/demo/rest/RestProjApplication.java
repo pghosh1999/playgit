663222e4cbeec39dd209ad59d126e4d26f109222
prasu <prasu@DESKTOP-C2G6710>
2021-05-15 11:15:40 -0400

    Initial commit

---
+++ Rest_Proj/src/main/java/com/example/demo/rest/RestProjApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestProjApplication.class, args);
+	}
+
+}
